e1016bf48ee91418e2286e1a0f74bd1356da59d5
Valentina Bustamante <vbustama@stevens.edu>
2021-02-27 20:32:45 -0500

    First version of us01 and us02 - both are currently working

--- milestone3/indi_fam_lists.py
+++ milestone3/indi_fam_lists.py
@@ -16,6 +16,9 @@ currDate = datetime.datetime.now()
 individuals = pd.DataFrame(columns = ['ID','Name','Gender','Birthday','Age','Alive','Death','Child','Spouse'])
 families = pd.DataFrame(columns = ['ID','Married','Divorced','Husband ID','Husband Name','Wife ID','Wife Name','Children'])
 
+#Errors list
+errors_us01 = []
+
 #define a function which determines the number of a month
 def monthNumber(month):
     months = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
@@ -27,8 +30,32 @@ birthFlag = False
 deathFlag = False
 marrFlag = False
 divFlag = False
+
+# keeps track of current line number in GED file
+counter = 0
+
+# checks if birth happened before the current date. if not, error message prints
+def birth_before_current_check(birth, id, counter):
+    birth_datetime = datetime.datetime(int(birth[2]), monthNumber(birth[1]), int(birth[0]))
+    if birth_datetime < currDate:
+        errors_us01.append("ERROR: INDIVIDUAL: US01: {}: {}: Birthday {} occurs in the future".format(counter, id, birth_datetime.date()))
+
+# checks if birth happened before marriage. if not, error message prints
+def birth_before_marriage_check(individuals2, families2):
+    errors_us02 = []
+    for indi_id, birth in zip(individuals2.index.tolist(), individuals2["Birthday"]):
+        for marriage_date, husb_id, wife_id in zip(families2["Married"], families2["Husband ID"], families2["Wife ID"]):
+            if indi_id == husb_id or indi_id == wife_id:
+                if isinstance(marriage_date, list):
+                    birth_dt = datetime.datetime(int(birth[2]), monthNumber(birth[1]), int(birth[0]))
+                    marriage_dt = datetime.datetime(int(marriage_date[2]), monthNumber(marriage_date[1]), int(marriage_date[0]))
+                    if birth_dt < marriage_dt:
+                        errors_us02.append("ERROR: INDIVIDUAL: US02: {}: Married {} before born {}".format(indi_id, marriage_dt.date(), birth_dt.date()))
+    return errors_us02
+
 #looping through lines in the gedcom file
 for line in file:
+    counter = counter + 1
     words = line.split() #create an array of the words
     if 'INDI' in words[-1]: #filter for individuals
         if row != {}: #this filters for the base case where no data is in row yet
@@ -63,6 +90,7 @@ for line in file:
             individuals = individuals.append(row, ignore_index=True)
             row = {} #reset dictionary
         row = {'ID': words[1][1:-1]} #start new row
+
     elif 'FAM' in words[-1]: #filter for families
         if str(individuals.index[0])[0] != 'I': #insert the last individual in the dataframe
             individuals = individuals.append(row, ignore_index=True)
@@ -107,6 +135,7 @@ for line in file:
             deathFlag = False
         elif birthFlag == True:
             row["Birthday"] = words[2:]
+            birth_before_current_check(row['Birthday'], row['ID'], counter) #US01: birth < current date check
             birthFlag = False
         elif marrFlag == True:
             row["Married"] = words[2:]
@@ -114,18 +143,31 @@ for line in file:
         elif divFlag == True:
             row["Divorced"] = words[2:]
             divFlag = False
+
+        
 row["Children"] = str(row['Children']) + '}' #add final child for final row of families
 families = families.append(row, ignore_index=True)
 #print and send to csv
 individuals.to_csv('individuals.csv')
 families.to_csv('families.csv')
-print(individuals.head(10))
-print(families.head(10))
+print("\nPeople:\n", individuals.head(10), "\n")
+print("Families:\n", families.head(10))
 file.close()
+
+# Prints out errors for birth before current day check: US01
+for error in errors_us01:
+    print(error)
+
+# Prints out errors for birth before marriage check: US02
+errors_us02 = birth_before_marriage_check(individuals, families)
+for error in errors_us02:
+    print(error)
+
+
 
 #part 2: print identifiers and names
 individuals = individuals.rename(columns={'Name': 'Names'})
-print('List of individuals with ID and Name')
+print('\nList of individuals with ID and Name')
 for index, row in individuals.iterrows():
     print('ID: ' + index + '    Name:', row['Names'])
 print()
