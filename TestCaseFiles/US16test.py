1f5eea9327db442ced06ea78e09c0ba33fef6579
dfaustin <dfaustin@stevens.edu>
2021-03-28 17:10:40 -0400

    Added US15test.py

--- TestCaseFiles/US16test.py
+++ TestCaseFiles/US16test.py
@@ -13,9 +13,10 @@ def MaleLastNames(fam_row, indi_df):
 
     for index, row in indi_df.iterrows():
     #try:
-        if row['Gender'] == 'M' and row['ID'] in children_list:
-            if row['Name'][1] != husband_last_name:
-                errors.append("ERROR: FAMILY: US16: {}: Child {} does not have father's last name".format(fam_row["ID"], row['ID']))
+        if isinstance(children_list, str):          #may be a list
+            if row['Gender'] == 'M' and row['ID'] in children_list:
+                if row['Name'][1] != husband_last_name:
+                    errors.append("ERROR: FAMILY: US16: {}: Child {} does not have father's last name".format(fam_row["ID"], row['ID']))
 
     #print(errors)
     return errors
@@ -24,9 +25,9 @@ def MaleLastNames(fam_row, indi_df):
 class Test(unittest.TestCase):
 
     def testMaleLastNames(self):
-        file = pd.read_csv('./Data/families.csv')
+        file = pd.read_csv('../Data/families.csv')
         for index, row in file.iterrows():
-            self.assertEqual(MaleLastNames(row, pd.read_csv('./Data/individuals.csv')), [])
+            self.assertEqual(MaleLastNames(row, pd.read_csv('../Data/individuals.csv')), [])
 
     
 
