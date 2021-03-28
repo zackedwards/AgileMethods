738f5980ec2adcf5dde15b34e60fa677a8d128ca
Valentina Bustamante <vbustama@stevens.edu>
2021-03-28 14:07:49 -0400

    Final version of US10 and US09 for sprint 2

--- TestCaseFiles/US10test.py
+++ TestCaseFiles/US10test.py
@@ -2,6 +2,10 @@
 Created on March 1st, 2021
 
 @author: Valentina Bustamante
+
+Descripion: Marriage should be at least 
+14 years after birth of both spouses 
+(parents must be at least 14 years old)
 '''
 import unittest
 import pandas as pd
@@ -10,7 +14,7 @@ import sys
 sys.path.insert(0, '../AgileMethods')
 
 from ast import literal_eval
-from functions import monthNumber
+from functions import monthNumber, convertStringToDatetime
 
 #get marriage date and both ages of the people married
 def marriageAfterFourteen(individuals2, families2):
@@ -21,13 +25,10 @@ def marriageAfterFourteen(individuals2, families2):
                 if isinstance(row_fam["Married"], str):
                     # compare marriage and birthdate and get the difference
                     # if difference is less than 14 then print an error
-                    birth = literal_eval(row_indi['Birthday'])
-                    birth_dt = datetime.datetime(int(birth[2]), monthNumber(birth[1]), int(birth[0]))
-                    marriage = literal_eval(row_fam['Married'])
-                    marriage_dt = datetime.datetime(int(marriage[2]), monthNumber(marriage[1]), int(marriage[0]))
+                    birth_dt = convertStringToDatetime(row_indi['Birthday'])
+                    marriage_dt = convertStringToDatetime(row_fam['Married'])
                     duration_in_s = (marriage_dt - birth_dt).total_seconds()
                     years = divmod(duration_in_s, 31536000)[0]
-                    #print(years)
                     if years < 14:
                         errors.append("ERROR: INDIVIDUAL: US10: {}: Married {} before age 14".format(row_indi["ID"], marriage_dt.date()))
     return errors
@@ -39,5 +40,4 @@ class Test(unittest.TestCase):
         self.assertEqual(marriageAfterFourteen(pd.read_csv('./Data/individuals.csv'), pd.read_csv('./Data/families.csv')), [])
 
 if __name__ == "__main__":
-    #print(file.head())
     unittest.main()
