ad9fcb5aecbf95857fe94bc356f60b1b2f43dcef
dfaustin <dfaustin@stevens.edu>
2021-04-11 18:49:16 -0400

    Added US23test.py and US24test.py

---
+++ TestCaseFiles/US23test.py
@@ -0,0 +1,28 @@
+'''
+Created on Apr 9, 2021
+
+@author: Danielle Faustino
+'''
+import unittest
+import pandas as pd
+
+def UniqueNameAndBirth(indi_df):
+    errors = []
+    seen = []
+
+    for index, row in indi_df.iterrows():
+        #try:         
+        if (row['Name'], row['Birthday']) in seen:
+            errors.append("ANOMOLY: INDIVIDUAL: US23: {}: Does not have unique name {} and birthday {}".format(row["ID"], row['Name'], row['Birthday']))
+        else:
+            seen.append((row['Name'], row['Birthday']))
+                    
+    #print(errors)
+    return errors
+
+class Test(unittest.TestCase):
+    def testUniqueNameAndBirth(self):
+        self.assertEqual(UniqueNameAndBirth(pd.read_csv('../Data/individuals.csv')), [])
+
+if __name__ == "__main__":
+    unittest.main()
