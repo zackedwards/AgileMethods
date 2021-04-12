e7b58c77ae5e6b1562b2963292f9175f872cfa7e
Zack Edwards <zedwards@stevens.edu>
2021-04-11 20:19:57 -0400

    adding us20

--- TestCaseFiles/US20test.py
+++ TestCaseFiles/US20test.py
@@ -3,34 +3,31 @@ Created on April 11th, 2021
 
 @author: Zack Edwards
 
-Descripion: Creates error if first cousins are married)
+Descripion: Creates error if aunts/uncles are married to neice/nephew
 '''
 import unittest
 import pandas as pd
 import sys
-sys.path.insert(0, '../AgileMethods')
 
 # Checks if husband id and wife id from a family are both in the children list of siblings in a list called row_fam
 def cousins_married_check(families, row_fam):
-    children = row_fam["Children"] #aunts and uncles
+    aunts_uncle = row_fam["Children"] #aunts and uncles
     errors = []
-    if not isinstance(children ,float):
+    if not isinstance(aunts_uncle ,float):
         #getting a list of cousins
-        cousins=[]
         all_cousins=[]
         for index, row_fam1 in families.iterrows():
             if isinstance(row_fam1["Wife ID"],str) and isinstance(row_fam1["Husband ID"],str):
-                if row_fam1["Wife ID"] or row_fam1["Husband ID"] in children: #if hus or wif in the list of aunts/uncles
+                if row_fam1["Wife ID"] or row_fam1["Husband ID"] in aunts_uncle: #if hus or wif in the list of aunts/uncles
                     if not isinstance(row_fam1['Children'], float):
-                        cousins.append(row_fam1['Children']) #append a list fo their kids to my list of cousins
                         for i in row_fam1['Children']:
-                            all_cousins.append(i)
+                            all_cousins.append(i) #add cousins to a list
 
-        if len(cousins) >= 2:
-            for index, row_fam2 in families.iterrows():
-                if isinstance(row_fam2["Wife ID"],str) and isinstance(row_fam2["Husband ID"],str):
-                    if row_fam2["Husband ID"] in all_cousins and row_fam2["Wife ID"] in all_cousins:
-                        errors.append("ERROR: FAMILY: US19: {}: Spouses {} and {} are cousins in family {}".format(row_fam2["ID"], row_fam2["Wife ID"], row_fam2["Husband ID"], row_fam["ID"]))
+        for index, row_fam2 in families.iterrows():
+            if isinstance(row_fam2["Wife ID"],str) and isinstance(row_fam2["Husband ID"],str):
+                if row_fam2["Husband ID"] in all_cousins or row_fam2["Wife ID"] in all_cousins:
+                    if row_fam2["Husband ID"] in aunts_uncles or row_fam2["Wife ID"] in aunts_uncles:
+                        errors.append("ERROR: FAMILY: US20: {}: Spouses {} and {} are aunt/uncle and niece/nephew in family {}".format(row_fam2["ID"], row_fam2["Wife ID"], row_fam2["Husband ID"], row_fam["ID"]))
         return errors
     else: 
         return []
