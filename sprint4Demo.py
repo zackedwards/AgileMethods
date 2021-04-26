82838e80d081b803c6495850ad199a16307101a9
willescamilla <wescamilla10@gmail.com>
2021-04-26 19:16:17 -0400

    Added US 29 & 30 to sprint4Demo

--- sprint4Demo.py
+++ sprint4Demo.py
@@ -7,8 +7,8 @@ from US26test import consistent_individuals_and_families #Takes in familiesDF & 
 from US27test import include_age_check #Takes in individualsDF
 
 from US28test import sibling_age_check #Takes in familiesDF and individualsDF
-from US29test import correct_gender_check #Takes in individualsDF & familiesDF
-from US30test import unique_ids_check #Takes in individualsDF & familiesDF
+from US29test import listDeceased #Takes in individualsDF
+from US30test import listLivingAndMarried #Takes in familiesDF & individualsDF
 
 from US31test import ListLivingSingle #Takes in individualsDF
 from US32test import ListMultipleBirths #Takes in familiesDF
@@ -23,6 +23,8 @@ for index, famRow in families.iterrows():
 errors += consistent_individuals_and_families(individuals, families)
 errors += include_age_check(individuals)
 errors += sibling_age_check(families, individuals)
+errors += listDeceased(individuals)
+errors += listLivingAndMarried(families, individuals)
 errors += ListLivingSingle(individuals)
 errors += ListMultipleBirths(families)
 
